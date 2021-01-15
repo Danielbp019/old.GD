89c892dd80d485207e96110ca35244cf1d4e2c09
Danielbp019 <76835848+Danielbp019@users.noreply.github.com>
2021-01-15 00:19:42 -0500

    efectos index
    efectos añadidos al index slide anim, boton de subir y smooth scroling

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,18 @@ $(document).ready(function () {
         } // End if
     });
 });
+
+/* Video en el jumbotron
+ -------------------------------------------------- */
+function deferVideo() {
+    //defer html5 video loading
+    $("video source").each(function () {
+        var sourceFile = $(this).attr("data-src");
+        $(this).attr("src", sourceFile);
+        var video = this.parentElement;
+        video.load();
+        // uncomment if video is not autoplay
+        //video.play();
+    });
+}
+window.onload = deferVideo;
